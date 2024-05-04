c3a1ee4b445db692755a1a81e0c921682ca9ff52
HaTungAnh <21021453@vnu.edu.vn>
2024-05-04 15:10:25 +0700

    refactor(SearchBar)

--- app/src/main/java/com/example/prj02_healthy_plan/ui/theme/ExploreScreen.kt
+++ app/src/main/java/com/example/prj02_healthy_plan/ui/theme/ExploreScreen.kt
@@ -1,132 +1,145 @@
 package com.example.prj02_healthy_plan.ui.theme
 
-import android.annotation.SuppressLint
-import android.health.connect.datatypes.HeartRateRecord
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.ShoppingCart
-import androidx.compose.material.icons.outlined.ShoppingCart
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.History
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.example.prj02_healthy_plan.R
-import com.example.prj02_healthy_plan.activities.ExploreViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChienTa(nav: NavHostController) {
-    val viewModel = viewModel<ExploreViewModel>()
-    val searchText by viewModel.searchText.collectAsState()
-    val persons by viewModel.persons.collectAsState()
-    val isSearching by viewModel.isSearching.collectAsState()
-
     Scaffold(
         topBar = {
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = Color(245, 250, 255)),
+            TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(
+                    245,
+                    250,
+                    255
+                )
+            ),
                 title = {
-                    Text("Explore", style = TextStyle(fontSize = 25.sp, fontWeight = FontWeight.Bold))
+                    Text(
+                        "Explore",
+                        style = TextStyle(
+                            fontSize = 25.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
                 },
                 navigationIcon = {
                     IconButton(onClick = { nav.popBackStack() }) {
                         Icon(Icons.Default.ArrowBack, contentDescription = "Back")
                     }
-                }
-            )
-        }
-    ) {innerPadding ->
+                })
+        }) { innerPadding ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
-                .padding(16.dp)
                 .background(Color(245, 250, 255))
         ) {
-            TextField(
-                value = searchText,
-                onValueChange = viewModel::onSearchTextChange,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .clip(RoundedCornerShape(40.dp))
-                    .border(2.dp, Color.Black, RoundedCornerShape(40.dp)),
-                placeholder = {
-                    Icon(
-                        painter = painterResource(R.drawable.baseline_search_24),
-                        contentDescription = "Search",
-                        tint = Color.Black,
-                        modifier = Modifier.size(28.dp))
-                })
+            SearchBar()
+        }
+    }
+}
 
-            Spacer(modifier = Modifier.height(16.dp))
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar() {
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+    var items = remember {
+        mutableStateListOf(
+            "Ha Tung Anh",
+            "Ta Hoang Giang",
+            "Ta Quang Chien",
+            "Doan Minh Hoang",
+            "Dong Van Duong"
+        )
+    }
 
-            if (isSearching) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    CircularProgressIndicator(
-                        modifier = Modifier.align(Alignment.Center)
-                    )
-                }
-            } else {
-                LazyColumn(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                ) {
-                    items(persons) { person ->
-                        Text(text = "${person.firstName} ${person.lastName}",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(vertical = 16.dp))
-                    }
-                }
+    SearchBar(
+        modifier = Modifier
+            .fillMaxWidth(),
+        query = text,
+        onQueryChange = {
+            text = it
+        },
+        onSearch = {
+            items.add(text)
+            active = false
+        },
+        active = active,
+        onActiveChange = {
+            active = it
+        },
+        placeholder = {
+            Text(text = "Search")
+        },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon"
+            )
+        },
+        trailingIcon = {
+            if (active) {
+                Icon(
+                    modifier = Modifier.clickable {
+                        if (text.isNotEmpty()) {
+                            text = ""
+                        } else {
+                            active = false
+                        }
+                    },
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Close Icon"
+                )
+            }
+        }
+    ) {
+        items.forEach {
+            Row(modifier = Modifier.padding(all = 14.dp)
+            ) {
+                Icon(
+                    modifier = Modifier.padding(end = 10.dp),
+                    imageVector = Icons.Default.History,
+                    contentDescription = "History Icon"
+                )
+
+                Text(text = it)
             }
         }
     }
