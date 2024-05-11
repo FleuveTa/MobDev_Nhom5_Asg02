32da5105a24d4b292eb24e445cdb57336e5f45f4
FleuveTa <hoanggiangvt003@gmail.com>
2024-05-11 15:59:40 +0700

    completed function

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.8.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-storage")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("com.google.android.gms:play-services-auth:21.1.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
