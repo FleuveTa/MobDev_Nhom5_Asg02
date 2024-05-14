c59a66bb4ef5819c40e99c4925a21a4cdf933038
HaTungAnh <21021453@vnu.edu.vn>
2024-05-14 20:33:02 +0700

    Modifier(UserViewModel)

--- app/src/main/java/com/example/prj02_healthy_plan/uiModel/UserViewModel.kt
+++ app/src/main/java/com/example/prj02_healthy_plan/uiModel/UserViewModel.kt
@@ -2,11 +2,8 @@ package com.example.prj02_healthy_plan.uiModel
 
 import android.util.Log
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.prj02_healthy_plan.RecipeFirebase
 import com.example.prj02_healthy_plan.User
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
@@ -14,9 +11,6 @@ import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
@@ -32,13 +26,13 @@ class UserViewModel : ViewModel() {
     private fun getUserInfor() {
         viewModelScope.launch {
             if (uId != null) {
-                state.value = fetchRecipes(uId = uId)
+                state.value = fetchUserInfor(uId = uId)
             }
         }
     }
 }
 
-suspend fun fetchRecipes(uId: String): User {
+suspend fun fetchUserInfor(uId: String): User {
     val db = Firebase.firestore
     var user = User()
 
