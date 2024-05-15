ebd1a7e44f77d134cac517c9fd54b38c410c306e
FleuveTa <hoanggiangvt003@gmail.com>
2024-05-15 23:57:21 +0700

    fetch and display daily data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation ("androidx.compose.runtime:runtime-livedata:1.7.0-beta01")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
