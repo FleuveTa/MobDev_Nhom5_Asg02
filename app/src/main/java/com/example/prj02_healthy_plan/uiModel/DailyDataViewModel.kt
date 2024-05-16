96004f37b35645e7751bbf1f74954640d3bf506b
FleuveTa <hoanggiangvt003@gmail.com>
2024-05-16 13:00:09 +0700

    user add food

--- app/src/main/java/com/example/prj02_healthy_plan/uiModel/DailyDataViewModel.kt
+++ app/src/main/java/com/example/prj02_healthy_plan/uiModel/DailyDataViewModel.kt
@@ -1,14 +1,18 @@
 package com.example.prj02_healthy_plan.uiModel
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.prj02_healthy_plan.DailyData
 import com.example.prj02_healthy_plan.RecipeFirebase
+import com.example.prj02_healthy_plan.RecipeInDaily
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -51,6 +55,23 @@ class DailyDataViewModel : ViewModel() {
             db.collection("dailyData")
                 .document(_dailyData.value.id?:"")
                 .update("water", _dailyData.value.water)
+        }
+    }
+
+    fun updateMeal(mealType: String, date: String, recipe: RecipeInDaily, context: Context) {
+        //val recipeRef = db.collection("recipes").document("recipeId")
+        viewModelScope.launch {
+            val dailyDataRef = db.collection("dailyData")
+            val query = dailyDataRef
+                .whereEqualTo("user", userRef)
+                .whereEqualTo("date", date)
+            query.get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        document.reference.update(mealType, FieldValue.arrayUnion(recipe))
+                    }
+                    Toast.makeText(context, "Added to $mealType", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
